25ba915c9eabaa595f97530cc47d2c70273dd353
Kakarla Durga Prasad <108335239+Durgaprasad-kakarla@users.noreply.github.com>
2024-01-19 19:48:22 +0530

    Update temple run.py

--- temple run.py
+++ temple run.py
@@ -7,38 +7,40 @@ from pynput.keyboard import Key,Controller
 cap=cv2.VideoCapture(0)
 detector=pem.poseDetector()
 keyboard=Controller()
-delayCounter=0
+counter=0
 while True:
     success,img=cap.read()
+    img=cv2.resize(img,(750,550))
     img=cv2.flip(img,1)
     detector.findPose(img,draw=False)
     lmList=detector.getPosition(img,draw=False)
     if lmList:
         p1,p2=lmList[1][1:],lmList[23][1:]
         left,right=lmList[18][1:],lmList[19][1:]
-        l, _, _ = detector.findDistance(p1, p2,)
+        l, _, _ = detector.findDistance(p1, p2)
         l1, _, _ = detector.findDistance(left, right)
-        print(p1[1],l1)
-        if left[0]<200 and right[0]>500:
-            keyboard.press(Key.space)
+        # print(p1[1],l1)
+        # print(left[0],right[0])
+        # if left[0]<250 and right[0]>600:
+        #     keyboard.press(Key.space)
+        #     keyboard.release(Key.space)
         if l1<80:
-            if delayCounter==0:
+            if counter==0:
                 keyboard.press(Key.up)
                 keyboard.release(Key.up)
-        if left[0]<200:
+        if p1[1]> 250:
+            if counter==0:
+                keyboard.press(Key.down)
+                keyboard.release(Key.down)
+        if left[0]<150:
                keyboard.press(Key.left)
-        if right[0]>500:
-            keyboard.press(Key.right)
-        if l<250:
-            keyboard.press(Key.down)
-        else:
-            keyboard.release(Key.left)
-            keyboard.release(Key.right)
-            keyboard.release(Key.space)
-            keyboard.release(Key.down)
+               keyboard.release(Key.left)
+        if right[0]>600:
+                keyboard.press(Key.right)
+                keyboard.release(Key.right)
         # print(lmList)
-        delayCounter+=1
-    if delayCounter==10:
-        delayCounter=0
+        counter+=1
+    if counter==11:
+        counter=0
     cv2.imshow("Temple Run",img)
     cv2.waitKey(1)
